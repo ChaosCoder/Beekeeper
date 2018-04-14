39fa84e0fe978910be0d8c22993475038895bb00
Andreas Ganske <a.ganske@chaosspace.de>
2018-04-14 21:55:05 +0200

    Initial Commit

---
+++ AUSTracking/AUSTracking.h
@@ -0,0 +1,19 @@
+//
+//  AUSTracking.h
+//  AUSTracking
+//
+//  Created by Andreas Ganske on 14.04.18.
+//  Copyright © 2018 Andreas Ganske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AUSTracking.
+FOUNDATION_EXPORT double AUSTrackingVersionNumber;
+
+//! Project version string for AUSTracking.
+FOUNDATION_EXPORT const unsigned char AUSTrackingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AUSTracking/PublicHeader.h>
+
+
